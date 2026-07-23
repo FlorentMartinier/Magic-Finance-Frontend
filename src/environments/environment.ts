1e65275cd57d4e7d584d3f6c4026e3b9ff425a21
Florent Martinier <florent.martinier@free.fr>
2026-07-23 20:33:02 +0200

    Fix production url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://magic-finance-api.duckdns.org/api/v1',
+    apiUrl: 'https://magic-finance-api.duckdns.org',
     apiKey: 'OBy5H8k7nlvOlYWYHQkj4z2tlklcPVeF'
 };
